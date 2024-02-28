0c8dc1c5e050b856b623c8c2ac86d2c9c9aa2e0a
Talia Pulsifer <taliaAPulsifer@outlook.com>
2024-02-28 15:42:20 -0600

    Trying to get file structure correct to seperate frontend and backend

---
+++ backend/src/main/java/com/company/application/main.java
@@ -0,0 +1,11 @@
+package com.yourcompany.yourapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YourApplication.class, args);
+    }
+}
